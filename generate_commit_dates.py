ce5b20d9c7df05835c27a735d5dce54e04916d39
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-05-20 21:36:16 +0200

    feat: write commit dates to file

--- generate_commit_dates.py
+++ generate_commit_dates.py
@@ -16,6 +16,7 @@ def main() -> None:
     
     commit_bool_transpose = list(map(list, zip(*commit_bool)))
     
+    commit_dict = []
     commit_date = starting_date
     for row in range(1,len(commit_bool_transpose)):
         for commit in commit_bool_transpose[row]:
@@ -23,8 +24,17 @@ def main() -> None:
                 commit_amount = "1-4"
             else:
                 commit_amount = "more than 5"
-            print(commit_date,commit_amount)
+            commit_dict.append({"date": commit_date, "number of commits": commit_amount})
             commit_date = commit_date + timedelta(days=1) 
+    
+    fields = ["date", "number of commits"]
+    
+    filename = "commit_dates.csv"
+    
+    with open(filename, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        writer.writerows(commit_dict)    
             
 
 if __name__ == '__main__':
