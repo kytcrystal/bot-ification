c0ba8cff0c99d78b421d98d4e0e529d0cf0633d9
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-05-20 21:50:06 +0200

    refactor: clean up

--- generate_commit_dates.py
+++ generate_commit_dates.py
@@ -3,38 +3,49 @@ from datetime import datetime
 from datetime import timedelta 
 from datetime import date 
 
+DATE_KEY = "date"
+NUM_COMMIT_KEY = "number of commits"
+INPUT_FILE = 'crystal-github.csv'
+OUTPUT_FILE = "commit_dates.csv"
+
+
 def main() -> None:
 
-    csv_file = 'crystal-github.csv'
-    commit_bool_reader = csv.reader(open(csv_file))
+    commit_bool_reader = csv.reader(open(INPUT_FILE))
     
     commit_bool = list(commit_bool_reader)
-    starting_date_string = commit_bool[0][0]
-    starting_date = datetime.strptime(starting_date_string, "%Y-%m-%d").date()
+    start_date_string = commit_bool[0][0]
+    start_date = datetime.strptime(start_date_string, "%Y-%m-%d").date()
     
     commit_bool[0][0] = ""
-    
     commit_bool_transpose = list(map(list, zip(*commit_bool)))
     
+    commit_dict = extract_commit_dates(start_date, commit_bool_transpose)
+    
+    write_csv(OUTPUT_FILE, commit_dict) 
+    
+
+def extract_commit_dates(start_date, commit_bool_transpose):
     commit_dict = []
-    commit_date = starting_date
+    commit_date = start_date
     for row in range(1,len(commit_bool_transpose)):
         for commit in commit_bool_transpose[row]:
             if commit == "0":
                 commit_amount = "1-4"
             else:
                 commit_amount = "more than 5"
-            commit_dict.append({"date": commit_date, "number of commits": commit_amount})
+            commit_dict.append({DATE_KEY: commit_date, NUM_COMMIT_KEY: commit_amount})
             commit_date = commit_date + timedelta(days=1) 
-    
-    fields = ["date", "number of commits"]
-    
-    filename = "commit_dates.csv"
+    return commit_dict
+
+
+def write_csv(filename, commit_dict):
+    fields = [DATE_KEY, NUM_COMMIT_KEY]
     
     with open(filename, 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields)
         writer.writeheader()
-        writer.writerows(commit_dict)    
+        writer.writerows(commit_dict)   
             
 
 if __name__ == '__main__':
