29afd4c85d980d1353350573e1e45ee525983ba0
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-05-28 21:56:28 +0200

    chore: remove unnecessary code

--- bot.py
+++ bot.py
@@ -1,25 +1,9 @@
-import logging
 import os
 from datetime import date, datetime, timedelta
 import csv
 
-from telegram import Update, ForceReply, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.constants import ParseMode
-from telegram.ext import Application, CommandHandler, CallbackContext, CallbackQueryHandler, ContextTypes
-
-logger = logging.getLogger(__name__)
-
-# Store bot screaming status
-screaming = False
-
-# Pre-assign menu text
-FIRST_MENU = "<b>Menu 1</b>\n\nCheck if commit is needed."
-SECOND_MENU = "<b>Menu 2</b>\n\nCheck how many commits needed."
-
-# Pre-assign button text
-CHECK_BUTTON = "Commit Today?"
-BACK_BUTTON = "Back"
-GITHUB_LINK = "GitHub"
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.environ.get("BOT_TOKEN")
 if TOKEN == None:
@@ -31,7 +15,6 @@ async def check_by_date(update, keyword):
         value += timedelta(days=1) 
         
     commit_number = get_commit_number(value)
-
     await update.message.reply_text(
             text=f"You should make {commit_number} commits {keyword}"
         )  
@@ -43,7 +26,6 @@ async def check(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         return
 
     input_date = context.args[0]
-    
     if input_date in ["today", "tomorrow"]:
         await check_by_date(update, input_date)
         return
@@ -62,14 +44,12 @@ async def check(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         return
 
     commit_number = get_commit_number(input_date)
-
     await update.message.reply_text(
         text=f"You should make {commit_number} commits on {input_date}"
     ) 
  
 
 def get_commit_number(input_date) -> None:
-
     csv_file = 'commit_dates.csv'
     commit_dict = csv.DictReader(open(csv_file))
 
@@ -81,16 +61,15 @@ def get_commit_number(input_date) -> None:
 
     return commit_number
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Sends explanation on how to use the bot."""
-    await update.message.reply_text("Hi! Use /checktoday to check if commit is needed today :)")
+    await update.message.reply_text("Hi! Use /check to check if commit is needed today :)")
+
 
 def main() -> None:
-    """Run bot."""
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token(TOKEN).build()
 
-    # on different commands - answer in Telegram
     application.add_handler(CommandHandler(["start", "help"], start))
     application.add_handler(CommandHandler("check", check))
 
